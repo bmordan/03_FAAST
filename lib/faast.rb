64bafd0f1f69ec7a7d3096421e71ee82a93adb7c
Bernard Mordan <bernardmordan@gmail.com>
2014-09-21 15:20:02 +0100

    Faast system container created

--- lib/faast.rb
+++ lib/faast.rb
@@ -1,4 +1,15 @@
 class Faast
+
+  DEFAULT_STATIONS = 24
+  DEFAULT_TRAINS = 22
+
+  def initialize(options = {})
+    s = options.fetch( :stations, DEFAULT_STATIONS )
+    s.times {stations << :station}
+    t = options.fetch( :trains, DEFAULT_TRAINS )
+    t.times {trains << :train}
+  end
+
   def trains
     @trains ||= []
   end
@@ -6,4 +17,6 @@ class Faast
   def stations
     @stations ||= []
   end
+
+
 end
--- spec/faast_spec.rb
+++ spec/faast_spec.rb
@@ -1,8 +1,9 @@
 require 'faast'
 
 describe Faast do
-  let(:faast) { Faast.new     }
-  let(:train) { double :train }
+  let(:faast)   { Faast.new       }
+  let(:train)   { double :train   }
+  let(:station) { double :station }
 
   it "should contain a number of trains" do
     expect(faast.trains).to be_a(Array)
@@ -10,6 +11,11 @@ describe Faast do
 
   it "should contain a number of stations" do
     expect(faast.stations).to be_a(Array)
+  end
+
+  it "can be inialized with trains and stations" do
+    expect(faast.stations.count).to eq(24)
+    expect(faast.trains.count).to eq(22)
   end
 
 end
